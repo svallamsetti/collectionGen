4fafb77bf1d06966f190565679ee38d2619cb92c
Sai Dinesh Vallamsetti <Dinesh@Sais-MacBook-Air.local>
2019-02-03 18:06:36 -0500

    first draft with stubs

--- src/CollectionGen.php
+++ src/CollectionGen.php
@@ -2,7 +2,70 @@
 
 namespace Svallamsetti\CollectionGen;
 
-class CollectionGen
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputArgument;
+
+class CollectionGen extends GeneratorCommand
 {
-    // Build your next great package.
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:Collection {name : The required name of the transformer class}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'An awesome Collection Scaffolder';
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Collection';
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/collection.stub';
+    }
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the command.'],
+        ];
+    }
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Collections';
+    }
+    /**
+     * Replace the class name for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     * @return string
+     */
+    protected function replaceClass($stub, $name)
+    {
+        $stub = parent::replaceClass($stub, $name);
+        return str_replace('DummyCollection', $this->argument('name'), $stub);
+    }
+
 }
